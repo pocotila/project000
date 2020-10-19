eb1bd03796c2118d681e51911058f12b70699e13
pocotila <iulian.pocotila@gmail.com>
2020-10-19 13:09:45 +0300

    tema

--- tema.py
+++ tema.py
@@ -2,12 +2,16 @@
 # are un sir de caractere dat de utilizator.
 # Aceasta numarare sa se realizeze
 # cu ajutorul unui for fara a utiliza len(). La final afisati rezultatul.
-string = "Am fost la munte si mi-a placut"
+string = "Am fost la munte si am instalat cortul"
 count = 0
 for i in string:
     count = count+1
-print("Am fost la munte si mi-a placut = ", count)
+print("Am fost la munte si am instalat cortul=", count)
 
+#2.	Creati un program in care utilizatorul sa introduca o adresa de email
+# de formatul litere_sau_cifre@litere_sau_cifre.litere.
+# Validati acest sir de caractere si informati utilizatorul de raspuns.
+# @ sau .(punct) trebuie sa exista o singura data in sirul de caractere.
 
 
 

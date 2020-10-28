ada7250a7513462a8b18534e674757bbef673962
pocotila <iulian.pocotila@gmail.com>
2020-10-28 19:39:15 +0200

    functii curs

---
+++ functii.py
@@ -0,0 +1,22 @@
+# print()
+
+#def message():
+#    print('Numarul introdus este 2')
+#
+#message()
+#
+#message = 1
+#print(message)
+
+def message(numar_1, numar_2):
+    print(f'Numarul introdus este {numar_1} si al doilea numar este {numar_2}')
+
+message(3, 40)
+
+message(numar_2=3, numar_1=20)
+
+
+
+
+
+

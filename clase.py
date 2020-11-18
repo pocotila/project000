47ccd2b8238fd1ddd957c073d34c06c35978b212
pocotila <iulian.pocotila@gmail.com>
2020-11-18 20:47:05 +0200

    clase

--- clase.py
+++ clase.py
@@ -1,17 +1,53 @@
-class SuperClass:
-    def __init__(self, name):
-        self.name = name
+# class SuperClass:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __str__(self):
+#         return f'Numele meu este {self.name}'
+#
+# class SubClass(SuperClass):
+#     def __init__(self, name):
+#         # SuperClass.__init__(self, name)
+#         super().__init__(name)
+#
+#     def __str__(self):
+#         return f"Ma numesc {self.name}"
+#
+# obj = SubClass('Ovidiu')
+# print(obj)
 
-    def __str__(self):
-        return f'Numele meu este {self.name}'
+# class SuperClass:
+#     def __init__(self):
+#         self.super_variabila = 11
+#
+# class SubClass(SuperClass):
+#     def __init__(self):
+#         super().__init__()
+#         self.sub_variabila = 12
+#         self.super_variabila = 111
+#
+# obj = SubClass()
+# print(obj.sub_variabila)
+# print(obj.super_variabila)
+
+
+class SuperClass:
+    def __init__(self):
+        self.super_variabila = 11
+
+    def metoda1(self):
+        return 'superclass'
 
 class SubClass(SuperClass):
-    def __init__(self, name):
-        # SuperClass.__init__(self, name)
-        super().__init__(name)
+    def __init__(self):
+        super().__init__()
+        self.sub_variabila = 12
+        self.super_variabila = 111
 
-    def __str__(self):
-        return f"Ma numesc {self.name}"
+    def metoda1(self):
+        return 'subclasa'
 
-obj = SubClass('Ovidiu')
-print(obj)
+obj = SubClass()
+print(obj.sub_variabila)
+print(obj.super_variabila)
+print(obj.metoda1)

a846bb2e4988699ef45fa9f5e5c8b328429d9db2
pocotila <iulian.pocotila@gmail.com>
2020-11-25 19:23:25 +0200

    decoratori

---
+++ decoratori.py
@@ -0,0 +1,10 @@
+def aduna(tip=1):
+    def aritmetic(*a):
+        rez = 0
+        for i in a:
+            rez = += i /len(a)
+        return rez
+   def adunare(*a):
+       rez = 0
+       for i in a:
+        rez += 1
